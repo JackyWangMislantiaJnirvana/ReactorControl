394bfd741496bf3532684d73a59503488e540b6d
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2016-07-03 12:11:10 +0800

    Fixed the problem of "cannot push", Restore the repo

---
+++ heat.lua
@@ -0,0 +1,6 @@
+heat = {}
+function heat.test()
+	print("the API loder works great if you see this message")
+end
+
+return heat
--- main.lua
+++ main.lua
@@ -1,1 +1,26 @@
+sleepTime = 5
+modules = {}
 
+reactor = peripheral.wrap("nuclear_reactor_#") -- fill in the number
+monitor = peripheral.wrap("left")
+term.redirect(monitor)
+
+-- modules manager
+modules[1] = "/ReactorControl/heat.lua"
+
+-- modules loader
+count = #modules
+for i = 1, count do
+	isSuccess = os.loadAPI(modules[i])
+	if isSuccess then
+		print("[Load] "..modules[i])
+	end
+end
+
+-- main structure
+while true do
+	reactorTemp = heat.getTemp()
+  print("Reactor heat".." "..reactorTemps)
+  
+  sleep(sleepTime)
+end
